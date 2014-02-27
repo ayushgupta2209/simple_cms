20001947607fdb5910c465e6933b805376d99c95
AYUSH GUPTA <ayushguptadtu@gmail.com>
2014-02-27 10:10:47 +0530

    Tuts 05_01 - 05_03

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,5 +2,19 @@ class DemoController < ApplicationController
   layout false
   
   def index
+	#can use any on of the three
+	#render(:template => 'demo/hello')
+	#render('demo/hello')
+	render('hello')
   end
+  
+  def hello
+	render('index')
+  end
+  
+  def other_hello
+	redirect_to(:controller => 'demo', :action => 'index')
+  end
+	
+
 end
